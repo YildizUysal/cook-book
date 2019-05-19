c53c971ed896597204d3b9e0e0e9c7a77b0443be
Yıldız Uysal <merveyildizuysal@gmail.com>
2019-05-19 19:57:09 +0300

    Initial Commit

---
+++ Cook Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cook Book
+//
+//  Created by Yıldız Uysal on 19.05.2019.
+//  Copyright © 2019 Yıldız Uysal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
